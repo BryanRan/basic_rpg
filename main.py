fd28a6438a540dd6e3ba19a76ab1f5beadab1387
Bryan <harosonbryan@gmail.com>
2025-09-07 20:01:16 +0300

    Implémentation des Armes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Personnage import Personnage
+from Arme import Arme
 
-joueur1 = Personnage("Aragorn","Guerrier",10,100,15,5)
+epee_legendaire = Arme("Epée légendaire",10)
+joueur1 = Personnage("Aragorn","Guerrier",10,100,15,5,epee_legendaire)
 joueur2 = Personnage("Gandalf","Magicien",1,80,5,15)
 
 joueur1.afficher_info()
